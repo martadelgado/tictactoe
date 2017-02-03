8128efd1456e6cc702a23a349f9b93544e86789f
Marta Delgado <marta.e.delgado@gmail.com>
2017-02-03 10:23:50 +0100

    almost there

--- game.js
+++ game.js
@@ -1,14 +1,14 @@
-var grid = [
-  [0,0,0],
-  [0,0,0],
-  [0,0,0]
-];
+// var grid = [
+//           [1,2,3],
+//           [4,5,6],
+//           [7,8,9]
+// ];
 
 function makeGrid(){
   for (var i = 0; i < 3; i++) {
     var row = $("<tr>");
     for (var j = 0; j < 3; j++) {
-      var field = $("<td>").addClass("tile").attr("id", i+"-"+j);
+      var field = $("<td>").addClass("tile").attr("id", "tile"+i+"-"+j);
       row.append(field);
     }
     $("#game-board").append(row);
@@ -17,145 +17,59 @@ function makeGrid(){
 
 function threeRow() {
     if ($("#0-0").text() === $("#0-1").text() && $("#0-0").text() === $("#0-2").text() && $("#0-0").text() !== ""){
-        alert("You Win!");
+        setTimeout(function(){ alert("You Win!"); }, 300);
     }
     if ($("#1-0").text() === $("#1-1").text() && $("#1-0").text() === $("#1-2").text() && $("#1-0").text() !== "") {
-        alert("You Win!");
+        setTimeout(function(){ alert("You Win!"); }, 300);
     }
     if ($("#2-0").text() === $("#2-1").text() && $("#2-0").text() === $("#2-2").text() && $("#2-0").text() !== "") {
-        alert("You Win!");
+        setTimeout(function(){ alert("You Win!"); }, 300);
     }
     if ($("#0-0").text() === $("#1-0").text() && $("#0-0").text() === $("#2-0").text() && $("#0-0").text() !== ""){
-        alert("You Win!");
+        setTimeout(function(){ alert("You Win!"); }, 300);
     }
     if ($("#0-1").text() === $("#1-1").text() && $("#0-1").text() === $("#2-1").text() && $("#0-1").text() !== "") {
-        alert("You Win!");
+        setTimeout(function(){ alert("You Win!"); }, 300);
     }
     if ($("#0-2").text() === $("#1-2").text() && $("#0-2").text() === $("#2-2").text() && $("#0-2").text() !== "") {
-        alert("You Win!");
+        setTimeout(function(){ alert("You Win!"); }, 300);
     }
     if ($("#0-0").text() === $("#1-1").text() && $("#0-0").text() === $("#2-2").text() && $("#0-0").text() !== "") {
-        alert("You Win!");
+        setTimeout(function(){ alert("You Win!"); }, 300);
     }
     if ($("#0-2").text() === $("#1-1").text() && $("#0-2").text() === $("#2-0").text() && $("#0-2").text() !== "") {
-        alert("You Win!");
+        setTimeout(function(){ alert("You Win!"); }, 300);
     }
-    else if ($("#game-board .tile") === "X" || $("#game-board .tile") === "O") {
-        alert("Tie Game!");
-    }
+}
+
+var moves = 0;
+function countMoves() {
+  if (moves === 9) {
+    setTimeout(function(){ alert("Tie Game!"); }, 300);
   }
-
-
+}
 
 $(document).ready(function() {
   makeGrid();
-  var symbol = "X";
+  var img = document.createElement("img");
+  img.src = "pic3.png";
+  var img1 = document.createElement("img");
+  img1.src = "pic2.png";
 
   $(".tile").on("click", function(){
-    $(this).html(symbol);
-    if (symbol === "X"){
-      symbol = "O";
+    $(this).html(img1);
+    if (img1 === img1){
+      img1 = img;
     } else {
-      symbol = "X";
+      img = img1;
     }
     threeRow();
+    });
+    $(".tile").on("click", function(){
+      moves++;
+      countMoves();
     });
   });
   $(".newGame").click(function() {
     location.reload();
 });
-
-  //
-  // var playerOne = 1;
-  // var playerTwo = 2;
-  // if ( player_one == 1 ){
-  //     document.getElementById("#game-board").innerHTML = "X";
-  //     playerOne = 1;
-  //
-  // } else {
-  //     document.getElementById("#game-board").innerHTML = "O";
-  //     playerTwo = 2;
-  //   }
-  //
-// });
-
-
-
-
-// function markX() {
-//
-// }
-
-// function threeRow() {
-//   for ( var i =0; i<row.length; i++) {
-//     if ($("#0-0") === $("#0-1") && $("#0-0") === $("#0-2")){
-//         alert("You Win!");
-//     }
-//     else changePlayer();
-//     if ("1-0" === "1-1") {
-//       continue;
-//       }
-//       if ("1-0" === "1-2")  {
-//         console.log("You Win!");
-//     }
-//     else changePlayer();
-//     if ("2-0" === "2-1") {
-//       continue;
-//       }
-//       if ("2-0" === "2-2")  {
-//         console.log("You Win!");
-//   }
-//     else changePlayer();
-//   }
-// }
-//
-// function threeColumn() {
-//   for ( var j =0; j<column.length; j++) {
-//     if ("0-0" === "1-0") {
-//       continue;
-//       }
-//       if ("0-0" === "2-0")  {
-//         console.log("You Win!");
-//     }
-//     else changePlayer();
-//     if ("0-1" === "1-1") {
-//       continue;
-//       }
-//       if ("1-0" === "2-1")  {
-//         console.log("You Win!");
-//     }
-//     else changePlayer();
-//     if ("0-2" === "1-2") {
-//       continue;
-//       }
-//       if ("0-2" === "2-2")  {
-//         console.log("You Win!");
-//       }
-//       else changePlayer();
-//   }
-// }
-//
-// var player1 = 'X';
-// var player2 = 'O';
-
-// function changePlayer(){
-// if (player1 === true)   {
-//   return player2;
-// }
-// else {
-//   return player1;
-// }
-// }
-
-// function changePlayer(square){
-//
-// var player_one = 1;
-// if ( player_one == 1 ){
-//     document.getElementById(square).innerHTML = "X";
-//     player_one = 0;
-//
-// } else {
-//     document.getElementById(square).innerHTML = "O";
-//     player_one = 1;
-//   }
-// }
